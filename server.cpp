a78958d8f511f4b84a50e89d10a473df4f8e1976
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-06 23:41:23 +0900

    update

--- server.cpp
+++ server.cpp
@@ -2,60 +2,54 @@
 #include <boost/bind.hpp>
 #include <iostream>
 
-namespace asio = boost::asio;
-using asio::ip::tcp;
-
 class Server {
-    asio::io_service& io_service;
-    tcp::acceptor acceptor;
-    tcp::socket socket;
-    asio::streambuf receive_buffer;
+    boost::asio::io_context& io_context;
+    boost::asio::ip::tcp::acceptor acceptor;
+    std::vector<boost::asio::ip::tcp::socket> sockets;
+    boost::asio::streambuf receive_buffer;
 
    public:
-    Server(asio::io_service& io_service_) : io_service(io_service_), acceptor(io_service_), socket(io_service_) {}
+    Server(boost::asio::io_context& io_context_, const boost::asio::ip::tcp::endpoint& endpoint)
+        : io_context(io_context_), acceptor(io_context_, endpoint) {}
 
-    void accept(unsigned short port) {
-        auto end_point = tcp::endpoint(tcp::v4(), port);
-        acceptor.open(end_point.protocol());
-        acceptor.bind(end_point);
-        acceptor.listen();
+    void accept() {
+        acceptor.async_accept([this](boost::system::error_code error_code, boost::asio::ip::tcp::socket socket) {
+            if (error_code) {
+                std::cout << "accept failed: " << error_code.message() << std::endl;
+            } else {
+                std::cout << "accept succeeded: " << socket.remote_endpoint() << std::endl;
+            }
 
-        acceptor.async_accept(socket, boost::bind(&Server::on_accept, this, asio::placeholders::error));
+            sockets.emplace_back(std::move(socket));
+            receive(sockets.back());
+
+            accept();
+        });
     }
+    void receive(boost::asio::ip::tcp::socket& socket) {
+        boost::asio::async_read_until(socket, receive_buffer, '\n', [&](const boost::system::error_code& error_code, std::size_t size) {
+            if (error_code) {
+                std::cout << "receive failed: " << error_code.message() << std::endl;
+                socket.close();
 
-    void receive() {
-        boost::asio::async_read(socket, receive_buffer, asio::transfer_all(),
-                                boost::bind(&Server::on_receive, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
-    }
+            } else {
+                std::string data(boost::asio::buffer_cast<const char*>(receive_buffer.data()), size);
+                std::cout << "receive succeeded: " << data << std::endl;
 
-   private:
-    void on_accept(const boost::system::error_code& error_code) {
-        if (error_code) {
-            std::cout << "accept failed: " << error_code.message() << std::endl;
-            return;
-        }
-
-        receive();
-    }
-
-    void on_receive(const boost::system::error_code& error_code, size_t bytes_transferred) {
-        if (error_code && error_code != boost::asio::error::eof) {
-            std::cout << "receive failed: " << error_code.message() << std::endl;
-            return;
-        }
-
-        const char* data = asio::buffer_cast<const char*>(receive_buffer.data());
-        std::cout << data << std::endl;
-
-        receive_buffer.consume(receive_buffer.size());
+                receive_buffer.consume(receive_buffer.size());
+                receive(socket);
+            }
+        });
     }
 };
 
-int main() {
-    asio::io_service io_service;
-    Server server(io_service);
+int main(int argc, char* argv[]) {
+    boost::asio::io_context io_context;
 
-    server.accept(31400);
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), std::atoi(argv[1]));
+    Server server(io_context, endpoint);
 
-    io_service.run();
+    server.accept();
+
+    io_context.run();
 }
