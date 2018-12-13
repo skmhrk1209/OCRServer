f17d81f66e8db4e9d002fe882e26467312879720
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-14 03:51:48 +0900

    update

--- server.cpp
+++ server.cpp
@@ -4,7 +4,9 @@
 #include <boost/bind.hpp>
 #include <boost/regex.hpp>
 #include <iostream>
+#include <memory>
 #include <string>
+#include <thread>
 #include <vector>
 
 namespace py = pybind11;
@@ -12,8 +14,13 @@ namespace asio = boost::asio;
 
 class Server {
     struct Socket {
+        using Ptr = std::shared_ptr<Socket>;
         template <typename... Args>
         Socket(Args&&... args) : socket(std::forward<Args>(args)...) {}
+        template <typename... Args>
+        static Ptr create(Args&&... args) {
+            return std::make_shared<Socket>(std::forward<Args>(args)...);
+        }
         asio::ip::tcp::socket socket;
         std::string send_buffer;
         std::string receive_buffer;
@@ -22,7 +29,7 @@ class Server {
    private:
     boost::asio::io_context& io_context;
     boost::asio::ip::tcp::acceptor acceptor;
-    std::vector<Socket> sockets;
+    std::vector<Socket::Ptr> sockets;
 
     const std::string sos = "<s>";
     const std::string eos = "</s>";
@@ -37,49 +44,44 @@ class Server {
     }
 
     void accept() {
-        acceptor.async_accept([this](auto error_code, auto&& socket) {
+        acceptor.async_accept([this](auto error_code, auto&& sock) {
             if (error_code) {
                 std::cout << "accept failed: " << error_code.message() << std::endl;
             } else {
-                std::cout << "accept succeeded: " << socket.remote_endpoint() << std::endl;
-
-                sockets.emplace_back(std::move(socket));
-                receive(sockets.back());
+                std::cout << "accept succeeded: " << sock.remote_endpoint() << std::endl;
+                auto socket = Socket::create(std::move(sock));
+                sockets.emplace_back(socket);
+                receive(socket);
             }
-
             accept();
         });
     }
 
-    void send(Socket& socket, const std::string& string) {
-        socket.send_buffer = sos + string + eos;
+    void send(const Socket::Ptr& socket, const std::string& string) {
+        socket->send_buffer = sos + string + eos;
 
-        asio::async_write(socket.socket, asio::buffer(socket.send_buffer), [&](auto error_code, ...) {
+        asio::async_write(socket->socket, asio::buffer(socket->send_buffer), [=](auto error_code, ...) {
             if (error_code) {
                 std::cout << "send failed: " << error_code.message() << std::endl;
-
-                socket.socket.close();
+                socket->socket.close();
             } else {
                 std::cout << "send succeeded" << std::endl;
-
                 receive(socket);
             }
         });
     }
 
-    void receive(Socket& socket) {
-        asio::async_read_until(socket.socket, asio::dynamic_buffer(socket.receive_buffer), boost::regex(sos + ".*" + eos), [&](auto error_code, ...) {
+    void receive(const Socket::Ptr& socket) {
+        asio::async_read_until(socket->socket, asio::dynamic_buffer(socket->receive_buffer), boost::regex(sos + ".*" + eos), [=](auto error_code, ...) {
             if (error_code) {
                 std::cout << "receive failed: " << error_code.message() << std::endl;
-
-                socket.socket.close();
+                socket->socket.close();
             } else {
                 std::cout << "receive succeeded" << std::endl;
+                socket->receive_buffer.erase(socket->receive_buffer.begin(), socket->receive_buffer.begin() + sos.size());
+                socket->receive_buffer.erase(socket->receive_buffer.end() - eos.size(), socket->receive_buffer.end());
 
-                socket.receive_buffer.erase(socket.receive_buffer.begin(), socket.receive_buffer.begin() + sos.size());
-                socket.receive_buffer.erase(socket.receive_buffer.end() - eos.size(), socket.receive_buffer.end());
-
-                send(socket, server_app.attr("process")(socket.receive_buffer).cast<std::string>());
+                send(socket, server_app.attr("process")(socket->receive_buffer).cast<std::string>());
             }
         });
     }
@@ -87,11 +89,9 @@ class Server {
 
 int main(int argc, char* argv[]) {
     boost::asio::io_context io_context;
-
     boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), std::atoi(argv[1]));
     Server server(io_context, endpoint);
 
     server.accept();
-
     io_context.run();
 }
